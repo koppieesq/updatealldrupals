3bcae130fdd48f82a144172094bb9a02494c6beb
Batman <jordan@koplowicz.com>
2025-07-06 17:05:32 -0700

    something in progress?

--- RoboFile.php
+++ RoboFile.php
@@ -6,5 +6,27 @@
  */
 class RoboFile extends \Robo\Tasks
 {
-    // define public methods as commands
+  function updateme ($path) {
+    $this->io()->title("UPDATE ALL THE THINGS!!!");
+
+    // Load sites, commands, and path to webroot
+    $sites = $opts["sites"];
+    $commands = $opts["commands"];
+    $path = $path ? $path : "/var/www/d7/sites";
+
+    // Move to the sites directory.
+
+    foreach ($sites as $site) {
+      $this->io()->section($site);
+      // Move to the site subdirectory
+
+      // Run commands in sequence.
+      foreach ($commands as $key => $value) {
+        $this->say($key);
+        $this->_exec($value);
+      }
+    }
+
+    $this->io()->info("All done!  Pat yourself on the back for a job well done.");
+  }
 }
